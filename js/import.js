1d2f80cffe7838595c1a5649b8d94fa065f28031
yoshimura-takuma <65653677+yoshimura-takuma@users.noreply.github.com>
2022-09-02 20:02:00 +0900

    Create import.js

---
+++ js/import.js
@@ -0,0 +1,9 @@
+function header(){
+    $.ajax({
+        url: "/html/header.html",
+        cache: false,
+        success: function(html){
+            document.write(html);
+        }
+    });
+}
